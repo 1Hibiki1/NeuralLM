9681c84e871898326ecf5a847201ce820d28f432
Hibiki <tori96.1729@gmail.com>
2024-07-11 12:06:17 +0300

    upproj/downproj works well for neural attn layers, 1024-2048-1024 is better than 1024-1024-1024 but not by that much and is much slower so gonna stick to 1024-1024-1024 for now (will need to test/tune this)

--- src/NeuralLM.py
+++ src/NeuralLM.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from kan import KANLinear
 
+
 class VNN(nn.Module):
     def __init__(self, layer_config, device='cpu'):
         super(VNN, self).__init__()
@@ -23,7 +24,8 @@ class VNN(nn.Module):
         output = (output / torch.norm(output, p=2, dim=2, keepdim=True))
 
         return output
-    
+
+
 class NeuralLMBlock(nn.Module):
     def __init__(self, layer_config, dim, device='cpu'):
         super(NeuralLMBlock, self).__init__()
@@ -31,24 +33,27 @@ class NeuralLMBlock(nn.Module):
         up_proj_factor = 1.5
         self.up_proj = nn.Linear(dim, int(dim*up_proj_factor))
         self.down_proj = nn.Linear(int(dim*up_proj_factor), dim)
-    
+
     def forward(self, inputs):
         output = inputs
         output = self.vnn(output)
         output = self.up_proj(output)
         output = self.down_proj(output)
         return output
-    
+
+
 class NeuralLMEncoder(nn.Module):
     def __init__(self, layer_config, dim, n_blocks, device='cpu'):
         super(NeuralLMEncoder, self).__init__()
-        self.blocks = nn.ModuleList([NeuralLMBlock(layer_config, dim, device=device) for _ in range(n_blocks)])
-    
+        self.blocks = nn.ModuleList(
+            [NeuralLMBlock(layer_config, dim, device=device) for _ in range(n_blocks)])
+
     def forward(self, inputs):
         output = inputs
         for block in self.blocks:
             output = block(output)
         return output
+
 
 if __name__ == "__main__":
     inputs = torch.ones(4, 3).to('mps')
